281725f6de5d57a3bb4ea3d5f1c31db08a8feae2
Dipankar Mandal <mandaldipankar2004@gmail.com>
2026-04-21 09:27:13 +0530

    fix: update CORS origin to match the correct frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // ✅ CORS Middleware (MUST be before routes)
 app.use(
   cors({
-    origin: "https://ecomerce-x0yf.onrender.com", // frontend URL from Render
+    origin: "https://ecomerce-olive-seven.vercel.app/", // frontend URL from Render
     credentials: true,
   })
 );
